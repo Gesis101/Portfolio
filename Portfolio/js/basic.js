267759e49831f5ec5f90363cd921df2d53631232
Thabo Moyo <thabo.s.moyo@gmail.com>
2019-02-23 05:20:19 +0000

    Please enter the commit message for your changes. Lines starting  On branch master  Your branch is up-to-date with 'origin/master'.

--- Portfolio/js/basic.js
+++ Portfolio/js/basic.js
@@ -6,15 +6,17 @@ var el = document.querySelector("#welcome");
 
 switch(true){
     case hour>=12 && hour<18:
-    greeting = "Good afternoon";
+    greeting = "Good Afternoon";
     break;
     case hour>=18 && hour<=23 && min < 59:
-    greeting = "Good evening";
+    greeting = "Good Evening";
     break;
     case hour>=0 && hour<12:
-    greeting = "Good morning";
+    greeting = "Good Morning";
     break;
     default :
     greeting = "Welcome";
 }
+
+
 el.innerHTML = greeting;
